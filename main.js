3c939330e01a8aad91d600a43f6b1238d575461e
Diar Alman <90829303+James159758@users.noreply.github.com>
2025-01-14 09:07:08 +0500

    Update main.js

--- main.js
+++ main.js
@@ -5,137 +5,206 @@
     // @grant        none
     // ==/UserScript==
 
-    (function() {
-    function inputInformation(event, flag) {
-        const inputElement = event.target;
-        const nameOfActor = inputElement.value;
-        const dataId = inputElement.getAttribute("data-myid");
-        const previousElement = elements[dataId];
-        const nameOfCharacter = previousElement.querySelector("span[itemprop='name']").textContent;
+  (function() {
+    'use strict';
 
-        if (!dictOfActors.hasOwnProperty(nameOfActor)) {
-            dictOfActors[nameOfActor] = new Set();
+
+    class Input {
+        constructor(parent, id, table) {
+            this.parent = parent;
+            this.id = id;
+            this.element = document.createElement("input");
+            this.table = table;
+
+
+            return this;
         }
-        if (flag == 'update') {
+        config() {
+            this.element.setAttribute("type", "text");
+            this.element.addEventListener("focus", () => this.table.manageData(this.value, this.parent, "remove"));
+            this.element.addEventListener("focusout", () => this.table.manageData(this.value, this.parent, "update"));
+            this.element.style.setProperty("text-align", "center");
+            this.element.style.setProperty("font-weight", "bold");
+            this.element.style.setProperty("border", "none");
+            this.element.style.setProperty("border-radius", "0px");
 
-            if (!dictOfActors[nameOfActor].has(dataId)) {
-                dictOfActors[nameOfActor].add(dataId)
-            }
 
-        } else if (flag == 'remove') {
-            if (dictOfActors[nameOfActor].has(dataId)) {
-                dictOfActors[nameOfActor].delete(dataId);
-                if (dictOfActors[nameOfActor].size == 0) {
-                    delete dictOfActors[nameOfActor];
-                }
-            }
+            return this;
         }
-        refreshInformation();
+        init() {
+            this.parent.element.appendChild(this.element);
 
-        return null;
+
+            return this;
+        }
+        get value() {
+            return this.element.value;
+        }
     }
 
 
-    function getMax(dictOfActors) {
-        maxCount = 0;
-        for (actor of Object.keys(dictOfActors)) {
-            maxCount = Math.max(maxCount, dictOfActors[actor].size);
+    class Label {
+        constructor(element, id, table) {
+            this.element = element;
+            this.id = id;
+            this.table = table;
+            this.input = new Input(this, this.id, this.table).config().init();
+
+
+            return this;
         }
-        return maxCount;
+        get name() {
+            return this.element.querySelector("span[itemprop='name']").textContent;
+        }
     }
 
-    function refreshInformation() {
-        mainDIV.innerHTML = "";
-        maxLoop = getMax(dictOfActors);
-        table = document.createElement("table");
-        table.setAttribute("class", "mytable")
-        boldTR = document.createElement("tr");
-        for (let actor of Object.keys(dictOfActors)) {
-            th = document.createElement("th");
-            th.setAttribute("class", "myclass");
-            th.textContent = actor;
-            th.addEventListener("click", function(){
-                let name = this.textContent;
-                arrayIDs = Array.from(dictOfActors[name]);
-                arrayIDs.sort((a, b) => a - b);
-                if(arrayIDs.some(x => elements[x].classList.contains("highlighted"))){
-                    arrayIDs.map(x => elements[x].classList.remove("highlighted"));
-                } else {
-                    arrayIDs.map(x => elements[x].classList.add("highlighted"));
-                }
-            });
-            boldTR.appendChild(th)
+
+    class Table {
+        constructor() {
+            this.element = document.createElement("table");
+            this.memory = new Object();
+
+
+            return this;
         }
-        table.appendChild(boldTR);
-        for (let i = 0; i < maxLoop; i++) {
-            tr = document.createElement("tr");
-            for (actor of Object.keys(dictOfActors)) {
-                td = document.createElement("td");
-                td.setAttribute("class", "myclass");
-                nameOfCharacter = "";
-                arrayIDs = Array.from(dictOfActors[actor]);
-                arrayIDs.sort((a, b) => a - b);
-                if (arrayIDs[i] !== undefined) {
-                    ID = arrayIDs[i];
-                    element = elements[ID];
-                    nameOfCharacter = element.querySelector("span[itemprop='name']").textContent;
-                    td.setAttribute("data-idch", ID);
-                    td.addEventListener("click", function() {
-                        let idch = this.getAttribute('data-idch');
-                        let mElement = elements[idch];
-                        mElement.classList.add("highlighted");
-                        setTimeout(function() {
-                            console.log(mElement);
-                            mElement.classList.remove("highlighted");
-                        }, 4000);
+        config() {
+            this.element.style.setProperty("font-family", "arial, sans-serif");
+            this.element.style.setProperty("border-collapse", "collapse");
+            this.element.style.setProperty("width", "100%");
+
+
+            return this;
+        }
+        init() {
+            document.body.appendChild(this.element);
+
+
+            return this;
+        }
+        manageData(key, value, flag) {
+            if (!this.memory.hasOwnProperty(key)) {
+                this.memory[key] = new Set();
+            }
+
+            if (flag == "update") {
+                if (!this.memory[key].has(value)) {
+                    this.memory[key].add(value);
+                }
+            } else if (flag == "remove") {
+                if (this.memory[key].has(value)) {
+                    this.memory[key].delete(value);
+                }
+            }
+            if (this.memory[key].size == 0) {
+                delete this.memory[key];
+            }
+            console.log(this.memory);
+            console.log(this.getMax);
+            this.refreshTable();
+
+
+            return this;
+        }
+
+        refreshTable() {
+            this.element.textContent = "";
+            // let cloneOfMemory = structuredClone(this.memory);
+            // Object.keys(cloneOfMemory).forEach((actor) =>{
+            //     cloneOfMemory[actor] = Array.from(cloneOfMemory[actor]);
+            //     cloneOfMemory[actor].sort((a, b) => a - b);
+            // });
+
+
+            let boldTR = document.createElement("tr");
+            for (let actor of Object.keys(this.memory)) {
+                let th = document.createElement("th");
+                console.log(actor);
+                th.textContent = actor;
+
+                let namesOfCast = this.memory[actor]
+                th.addEventListener("click", function(){
+                    namesOfCast.forEach((e) =>{
+                        if(e.element.classList.contains("highlighted")){
+                            e.element.classList.remove("highlighted");
+                        } else {
+                            e.element.classList.add("highlighted");
+                            setTimeout(()=>{
+                                if(e.element.classList.contains("highlighted")) e.element.classList.remove("highlighted");
+                            }, 4000);
+                        }
                     });
-                }
-                td.textContent = nameOfCharacter;
-                tr.appendChild(td);
+
+
+                });
+
+                boldTR.appendChild(th);
             }
-            table.appendChild(tr);
+            this.element.appendChild(boldTR);
+
+            for (let i = 0; i < this.getMax; i++) {
+                let tr = document.createElement("tr");
+                for (let actor of Object.keys(this.memory)) {
+                    let td = document.createElement("td");
+                    let namesOfCast = Array.from(this.memory[actor]);
+                    namesOfCast.sort((a, b) => a.name - b.name);
+                    td.textContent = undefined;
+                    if(namesOfCast[i] !== undefined){
+                        td.textContent = namesOfCast[i].name;
+                        
+
+                        td.addEventListener("click", function(){
+                            if(namesOfCast[i].element.classList.contains("highlighted")){
+                                namesOfCast[i].element.classList.remove("highlighted");
+                            } else {
+                                namesOfCast[i].element.classList.add("highlighted");
+                                setTimeout(()=>{
+                                    if(namesOfCast[i].element.classList.contains("highlighted")) namesOfCast[i].element.classList.remove("highlighted");
+                                }, 4000);
+                            }
+                        });
+
+
+                    }
+
+                    tr.appendChild(td);
+                }
+                this.element.appendChild(tr);
+            }
+
+
+            return this;
         }
-        mainDIV.appendChild(table);
+
+        get getMax() {
+            let maxLength = 0;
+            for (let key of Object.keys(this.memory)) {
+                maxLength = Math.max(maxLength, this.memory[key].size);
+            }
+
+
+            return maxLength;
+        }
+
     }
 
-    function addingElements() {
-        elements = document.querySelectorAll(MODAL_SELECTOR);
-        for (let i = 0; i < elements.length; i++) {
-            inputE = document.createElement("input");
-            inputE.setAttribute('type', 'text');
-            inputE.setAttribute("class", "myinput");
-            inputE.setAttribute("data-myID", String(i));
-            inputE.addEventListener('focus', ((event) => inputInformation(event, "remove")));
-            inputE.addEventListener('focusout', ((event) => inputInformation(event, "update")));
-            elements[i].appendChild(inputE);
-        }
+
+
+
+    var listOfObjects = [];
+    let table = new Table()
+    table.config().init();
+
+    const characters = document.querySelectorAll("div[id^='charid_'], div[id^='crtid_']");
+
+
+    for (let i = 0; i < characters.length; i++) {
+        listOfObjects.push(new Label(characters[i], i, table));
     }
 
-    function changing(mutationsList, observer) {
-        for (let mutation of mutationsList) {
-            if (mutation.type === 'childList') {
-                const modalElement = document.querySelector(MODAL_SELECTOR);
-                if (modalElement) {
-                    observer.disconnect();
-                    let myStyle = document.createElement("style");
-                    myStyle.innerHTML = `
-    .myinput {
-        text-align: center;
-        font-weight: bold;
-        border: none;
-        border-radius: 0px;
-    }
-    .mytable {
-        font-family: arial, sans-serif;
-        border-collapse: collapse;
-        width: 100%;
-    }
-    .myclass {
-        border: 1px solid #dddddd;
-        text-align: left;
-        padding: 8px;
-    }
-    .highlighted {
+
+    let myStyle = document.createElement("style");
+    myStyle.innerHTML = `
+.highlighted {
     animation: mymove 8s infinite;
     animation-timing-function: linear;
   }
@@ -145,27 +214,7 @@
   50% {box-shadow: 0 0 0px 6px white;}
   75% {box-shadow: 0 0 0px 8px black;}
   100% {box-shadow: 0 0 0px 10px white;}
-}
-    `;
+}`
+    document.head.appendChild(myStyle);
 
-                    document.head.appendChild(myStyle);
-                    addingElements();
-                    break;
-                }
-            }
-        }
-    }
-
-    const observer = new MutationObserver(changing);
-    observer.observe(document.body, {
-        childList: true,
-        subtree: true
-    });
-    var dictOfActors = new Object();
-    const myID = "dontlookathere";
-    const mainDIV = document.createElement("div");
-    mainDIV.setAttribute("data-myID", myID);
-    document.body.appendChild(mainDIV);
-    const MODAL_SELECTOR = "div[class='g_bubble stripe medium']";
-    console.log("Observer started.");
 })();
